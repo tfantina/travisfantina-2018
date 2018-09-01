8cdbe1d3a139ed8e6edc1cc247fc52ac42141cc5
Travis Fantina <tfantina@gmail.com>
2018-09-01 18:17:31 -0400

    Parralax and scrolling adjustments to projects template

---
+++ projects.php
@@ -0,0 +1,28 @@
+<?php 
+/**
+  *Template Name: Projects Page
+  *@package WordPress 
+  *@subpackage tfavisfantina-2018
+  **/
+  
+   ?>
+   <?php get_header() ?>
+   <div class="empty" style="height: 163px; width: 100%;"></div>
+
+   <div class="project">
+   
+   </div>
+   
+   <div class="project">
+   
+   </div>
+   <div class="project">
+   
+   </div>
+   <div class="project">
+   
+   </div>
+   <div class="project">
+   
+   </div>
+  
