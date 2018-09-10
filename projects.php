adb03b25b689bb4360bd1a7140c576ddd6472a7e
Travis Fantina <tfantina@gmail.com>
2018-09-10 15:35:11 -0400

    Adding first projects as a test to the layout

--- projects.php
+++ projects.php
@@ -10,14 +10,76 @@
    <div class="empty" style="height: 163px; width: 100%;"></div>
 
    <div class="project">
+     <div class="project-template">
+     <div class="project-l"> <h2>Avalie Meu Professor</h2>
+       <p>
+         Served as Co-founder and CTO building a webapp to help Brazilian Students choose their professors 
+         and discuss their university experience.
+         
+       </p>
+       <p>Timeline Feb 2016 - Oct 2017</p>
+       <p>
+       Tools:
+       <ul>
+         <li>Ruby on Rails</li>
+         <li>Bootstrap</li>
+         <li>Sketch App</li>
+         <li>Heroku</li>
+       </ul>
+       </p>
+     </div>
+     <div class="project-r">Righthand</div>
+     </div>
+    
    
    </div>
    
    <div class="project">
+     <div class="project-template">
+     <div class="project-l"> <h2>FirstDraft Writer</h2>
+       <p>
+         Coming Soon - A truly distraction free writer that helps you get your thoughts on paper as fast as possible
+       </p>
+       <p>Timeline: July 2018 - Present</p>
+       <p>
+       Tools:
+       <ul>
+         <li>JavaScript</li>
+       </ul>
+       </p>
+     </div>
+     <div class="project-r">Righthand</div>
+     </div>
+    
    
    </div>
    <div class="project">
-   
+     <div class="project-template">
+     <div class="project-l"> <h2>Q&A Transcript Manager</h2>
+       <p>
+         Worked closely with the founder designing a minimum viable product based on their initial
+         ideas and specifications.  The application is designed to help Court Reporters manage their 
+         transcripts, set deadlines, budget their time and workload.  I was the lead designer on the 
+         product and took the design from the initial stages all the way to the handoff with the developer.
+         Throughout development I continnued to consult on design issues.  
+         </p>
+        <p>
+        After the initial launch I was asked to return to add aditional features informed by user feedback.
+        This gave me a unique oppertunity to reaccess my original designs and make changes almost a year later.
+        </p>
+       <p>Timeline: April - July 2017 & Febuary - March 2018</p>
+       <p>
+       Tools:
+       <ul>
+         <li>Sketch App</li>
+         <li>Illustrator</li>
+         <li>InVision</li>
+       </ul>
+       
+       </p>
+     </div>
+     <div class="project-r">Righthand</div>
+     </div>
    </div>
    <div class="project">
    
